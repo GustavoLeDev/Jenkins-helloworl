30a5e21e404b73a8cf3fbb347189181ceb4b74f0
gustavo <114112798+GustavoLeDev@users.noreply.github.com>
2023-06-27 07:16:29 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello Monde!");
-        System.out.println("GustavoLedev guerrie");
+        System.out.println("GustavoLedev guerrier");
     }
 }
